743dcb12bddbdf4c68136994a2f918650890eac1
iamphc <iamthephc@gmail.com>
2023-07-23 20:21:49 +0800

    doc: adjust file structure of vuepress docs

--- .vuepress/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  title: 'package assistant',
-  description: 'VSCode Auto Update Dependencies Extension',
-}
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: 'package assistant',
+  description: 'VSCode Auto Update Dependencies Extension'
+}
